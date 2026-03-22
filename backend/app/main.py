b79f98d5f2c41ff8e064e54fe5a0641a2e9d8084
Aryan Rajesh <aryanrajesh@aryans-macbook-5.local>
2026-03-22 13:58:19 -0700

    implemented code for supabase test

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,38 @@
 
 from fastapi import FastAPI
 from dotenv import load_dotenv
+import os
+from supabase import create_client
+from fastapi.middleware.cors import CORSMiddleware
+
 
 load_dotenv()
 
-newapp = FastAPI(title = "AI Traffic Signal Optimizer")
+app = FastAPI(title = "AI Traffic Signal Optimizer")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+supabase = create_client (
+    os.getenv("SUPABASE_URL"),
+    os.getenv("SUPAVASE_SERVICE_ROLE_KEY")
+)
+
+@app.get("/")
+async def root():
+    return {"status": "ok", "message": "AI Traffic Signal Optimizer API"}
+
+@app.get("/health")
+async def health():
+    return {"status": "healthy"}
+
+@app.get("/test-db")
+async def test_db():
+    result = supabase.table("intersections").select("*").execute()
+    return {"intersections": result.data}
